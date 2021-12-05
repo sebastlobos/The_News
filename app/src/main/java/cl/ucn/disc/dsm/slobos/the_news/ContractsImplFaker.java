163123857d88ad3a2d9a8285ef823f7f2ce3dcd5
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-12-05 01:47:34 -0300

    Feat(Contracts): Add the faker

--- app/src/main/java/cl/ucn/disc/dsm/slobos/the_news/ContractsImplFaker.java
+++ app/src/main/java/cl/ucn/disc/dsm/slobos/the_news/ContractsImplFaker.java
@@ -1,4 +1,58 @@
 package cl.ucn.disc.dsm.slobos.the_news;
 
-public class ContractsImplFaker {
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.github.javafaker.Faker;
+
+import org.jetbrains.annotations.Contract;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Contract Implementation of news With Faker
+ */
+
+public class ContractsImplFaker implements Contracts {
+    private int size;
+
+    /**
+     * @return all the News in the backend.
+     */
+
+    final List<News> newsList = new ArrayList<>();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public List<News> retrieveNews() {
+        
+      final Faker faker = new Faker();
+      for(int i = 0; i<size; i++){
+
+          News news = new News(
+                  faker.superhero().name(),
+                  faker.artist().name(),
+                  faker.artist().name(),
+                  faker.internet().url(),
+                  faker.internet().url(),
+                  faker.backToTheFuture().quote(),
+                  faker.starTrek().villain().length(),
+                  ZonedDateTime.now(ZoneId.of("-4"))
+          );
+          newsList.add(news);
+
+      }
+      return newsList;
+    }
+
+    @Override
+    public List<News> retrieveNews(int i) {
+
+
+        return null;
+    }
+
 }
