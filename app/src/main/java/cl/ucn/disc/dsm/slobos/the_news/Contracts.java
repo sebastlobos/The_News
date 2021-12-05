b3c877e4f8210d4421892b44a98282c6361220cf
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-12-05 01:00:14 -0300

    feat(project) : constructor added

---
+++ app/src/main/java/cl/ucn/disc/dsm/slobos/the_news/Contracts.java
@@ -0,0 +1,22 @@
+package cl.ucn.disc.dsm.slobos.the_news;
+
+import java.util.List;
+
+/**
+ * The contracts of the News Project
+ * @author Sebastian Lobos
+ */
+
+
+public interface Contracts {
+    /**
+     *
+     * @return all the News in the backend.
+     */
+
+
+
+List<News> retrieveNews();
+
+
+}
