95b34a99940ead720a3a0e3064b3f5fb1c1b4a43
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-12-08 00:32:05 -0300

    Fix(New): fix news parameters

--- app/src/test/java/cl/ucn/disc/dsm/slobos/the_news/TestNews.java
+++ app/src/test/java/cl/ucn/disc/dsm/slobos/the_news/TestNews.java
@@ -35,12 +35,17 @@ public  final class TestNews {
         {
 
             News news = new News(
-                    title"UCN hace pilotaje de instrumentos de evaluación diagnóstica de matemática y lenguaje",
-                source"https://www.noticias.ucn.cl/",
-                author"Sebastian Lobos",
-                urlImage"https://www.noticias.ucn.cl/destacado/ucn-hace-pilotaje-de-instrumentos-de-evaluacion-diagnostica-de-matematica-y-lenguaje/")
-                    description"Se busca medir conocimientos y habilidades elementales adquiridos por los estudiantes del Programa Propedéutico y PACE del plantel durante su formación escolar."
-                    content"Estudiantes del Programa Propedéutico y PACE de la Universidad Católica del Norte (UCN) entre el 18 y el 22 de noviembre,  "
+                    "Autoridades UCN informan a las unidades los detalles del Plan Retorno y piden acelerar regreso",
+                    "Noticias UCN",
+                    "UCN",
+                    "https://www.noticias.ucn.cl/destacado/autoridades-ucn-informan-a-las-unidades-los-detalles-del-plan-retorno-y-piden-acelerar-regreso/",
+                    "https://www.noticias.ucn.cl/wp-content/uploads/2021/11/WhatsApp-Image-2021-11-05-at-13.26.53-1.jpeg",
+                    "En reuniones presenciales realizadas en la Casa Central se expusieron las medidas adoptadas y se respondieron las dudas de los y las representantes de las distintas unidades de la Universidad.",
+                    "Directores/as y representantes de las distintas unidades académicas de Antofagasta de la Universidad" +
+                            " Católica del Norte (UCN), recibieron con aprobación los detalles del Plan Retorno que desarrolla nuestra Casa de Estudios. En dos reuniones presenciales realizadas en el auditorio Andrés Sabella de la Casa Central, el vicerrector académico, Nelson Fernández Vergara; la vicerrectora de Asuntos Económicos y Administrativos, Ingrid Álvarez Arzic; y la directora de la Dirección de Personas (ex Dirección de Recursos Humanos) Alejandra Pizarro Véliz, " +
+                            "les explicaron los detalles del proceso, respondiendo también las dudas de los y las asistentes.",
+
+
 
                     ZonedDateTime.now(ZoneId.of("-4"))
             );
